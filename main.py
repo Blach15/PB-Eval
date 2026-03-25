a1aa439207b37491cc8a94e175cb4bb0e1b6b058
Svend <202106825@post.au.dk>
2026-03-25 11:34:36 +0100

    outcome visualization

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
-from pabutools.election import Project, Instance, ApprovalBallot, ApprovalProfile, Cost_Sat
+from pabutools.election import Project, Instance, ApprovalBallot, ApprovalProfile, Cost_Sat, parse_pabulib
 from pabutools.rules import greedy_utilitarian_welfare, sequential_phragmen, method_of_equal_shares
-from pabutools.visualisation import GreedyWelfareVisualiser
+from pabutools.visualisation import GreedyWelfareVisualiser, MESVisualiser
+import os
 
-p1 = Project("p1", 1) 
-p2 = Project("p2", 1)
-p3 = Project("p3", 3)
+def visualise_election(filename=str, sat_class=Cost_Sat, rule="greedy", verbose=False):
+    path = os.path.join("./elections/", filename)
+    if verbose:
+        print(f"Parsing election instance from {path}...")
+    instance, profile = parse_pabulib(path)
+    if rule == "greedy":
+        if verbose:
+            print("Computing outcome using greedy utilitarian welfare...")
+        outcome = greedy_utilitarian_welfare(instance, profile, sat_class=sat_class, analytics=True)
+        visualiser = GreedyWelfareVisualiser(profile, instance, outcome)
+        if verbose:
+            print("Rendering visualisation...")
+        visualiser.render("./output/", name=os.path.basename(path).split(".")[0] + "_greedy_utilitarian_welfare")
+        if verbose:
+            print("Visualisation complete.")
+    elif rule == "mes":
+        if verbose:
+            print("Computing outcome using method of equal shares...")
+        outcome = method_of_equal_shares(instance, profile, sat_class=sat_class, analytics=True)
+        visualiser = MESVisualiser(profile, instance, outcome)
+        if verbose:
+            print("Rendering visualisation...")
+        visualiser.render("./output/", name=os.path.basename(path).split(".")[0] + "_method_of_equal_shares")
+        if verbose:
+            print("Visualisation complete.")
+    else:
+        raise ValueError(f"Unknown rule: {rule}")
 
-instance = Instance()
-instance.add(p1)
-instance.update([p2, p3])
-
-instance.budget_limit = 3
-
-b1 = ApprovalBallot([p1, p2])
-b1.add(p2)
-b2 = ApprovalBallot({p1, p2, p3})
-b3 = ApprovalBallot({p3})
-
-profile = ApprovalProfile([b1, b2])
-profile.append(b3)
-
-outcome1 = greedy_utilitarian_welfare(instance, profile, sat_class=Cost_Sat)
-
-print("Greedy Utilitarian Welfare Outcome:", outcome1)
-
-visualiser = GreedyWelfareVisualiser(profile, instance, outcome1)
-
-visualiser.render("./t/", name="test")
-
+visualise_election("netherlands_amsterdam_252_.pb", sat_class=Cost_Sat, rule="greedy", verbose=True)
+visualise_election("netherlands_amsterdam_252_.pb", sat_class=Cost_Sat, rule="mes", verbose=True)
 
 
 
